53c06978b19d8d88a634a4fa24f0c2de1cb99de0
fiery dusk <camsays.dub.tea.eff@gmail.com>
2018-04-09 20:04:26 +1200

    it looked ugly
    just moved my tabbing around

--- timestable.cs
+++ timestable.cs
@@ -1,11 +1,11 @@
 using System;
-namespace timestable
-{class Program{
-	static void Main(string[] args){
-		try{
-			for (int num = int.Parse(args[0]); num <= int.Parse(args[1]); num++){
-				for (int cur = 0; cur <= 10; cur++)
-					{Console.WriteLine(num + " * " + cur + " = " + (num*cur));}
-				Console.WriteLine("");}}
-		catch (Exception except){Console.WriteLine(except);}
+namespace timestable{
+	class Program{
+		static void Main(string[] args){
+			try{
+				for (int num = int.Parse(args[0]); num <= int.Parse(args[1]); num++){
+					for (int cur = 0; cur <= 10; cur++)
+						{Console.WriteLine(num + " * " + cur + " = " + (num*cur));}
+					Console.WriteLine("");}}
+			catch (Exception except){Console.WriteLine(except);}
 }}}
